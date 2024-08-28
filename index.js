adbe50c087c7e554389e567a0d5f17be69b140d7
Umar khan <thisisumaralii@gmail.com>
2024-08-28 18:42:35 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Hello world</h1>')
+})
+app.get('/youtube', (req, res) => {
+    res.send('<h2>Chai pelooo...</h2>')
+})
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
